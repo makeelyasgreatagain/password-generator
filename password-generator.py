b15a5342177a5b9a516e764f4ced7d113c832fc6
Elyas <elysignup@gmail.com>
2025-09-01 22:29:32 +0330

    Update password-generator.py
    Add password minimum length controller
    Create infinite loop for running app

--- password-generator.py
+++ password-generator.py
@@ -1,11 +1,15 @@
 import string
 import random
 
-length = int(input("Please Provide password length: "))
 charlist = string.ascii_letters + string.digits + string.punctuation
 password = ""
 
-for i in range(length):
-    password += random.choice(charlist)
-
-print("Genereted password is: " + password)
+while True:
+    length = int(input("Please Provide password length: "))
+    if length >= 8:
+        for i in range(length):
+            password += random.choice(charlist)
+        print("Genereted password is: " + password)
+        break
+    else:
+        print("I can't generate shorter than 8 characters password due to security reasons.")
